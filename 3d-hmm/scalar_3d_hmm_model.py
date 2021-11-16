27bdf59b55cb7d9fa5cf4b76db13d9a593a2b88e
Alex Lewis <alexlewisnc@gmail.com>
2021-11-16 01:37:17 -0500

    Test/complete forward method in scalar model

--- 3d-hmm/scalar_3d_hmm_model.py
+++ 3d-hmm/scalar_3d_hmm_model.py
@@ -3,9 +3,6 @@ import torch
 from torch import nn
 # from torch import tensor
 
-id = torch.cuda.current_device()
-cuda = "cuda:"+str(id)
-device = torch.device(cuda)
 
 def index2coord(xy_size):
     def f(state):
@@ -21,14 +18,24 @@ class TransitionModel(nn.Module):
         self.states = states
         self.transition_matrix_unnormalized = nn.Parameter(torch.randn(states, 7))
 
-    def log_p(self, state_next, state_prev, index2coord):
-        x, y, z = index2coord(state_next) - index2coord(state_prev)
+    def log_p(self, num_states, xy_size):
+        transitions = nn.functional.log_softmax(self.transition_matrix_unnormalized, dim=-1)
+
+        out = torch.zeros(num_states, num_states).log()
+
         neighbors = [(0, 0, 0), (1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, 2)]
-        if (x, y, z) in neighbors:
-            # TODO is this correct, using `state_prev`?
-            transitions = nn.functional.log_softmax(self.transition_matrix_unnormalized[state_prev], dim=-1)
-            return transitions[neighbors.index((x,y,z))]
-        return torch.tensor(0).log() # TODO
+        for h in range(num_states):
+            # print(index2coord(xy_size)(h))
+
+            # TODO distribution is not always over seven states!!!
+            for idx, neighbor in enumerate(neighbors):
+                # i = coord2index(index2coord(h) - neighbor)
+                x, y, z = neighbor
+                i = (h - (x + xy_size * (y + xy_size * z))) % num_states  # TODO don't wrap, but check bounds
+                # print("\t", neighbor, "=>", i)
+                out[h, i] = transitions[h, idx]
+
+        return out
 
 
 
@@ -41,10 +48,14 @@ class EmissionModel(nn.Module):
         self.emission_matrix_unnormalized = nn.Parameter(torch.randn(num_states, num_tokens))
 
     def log_p(self, sentences_tensor):
-        emission_matrix = nn.functional.softmax(self.emission_matrix_unnormalized, dim=1)
-        # sentence_tensor = sentence_tensor[sentence_tensor > -1]
+        # returns matrix with shape num_batches x num_states
+
+        # sentences_tensor = sentences_tensor.clamp(max=11)  # TODO testing
+
+        emission_matrix = nn.functional.log_softmax(self.emission_matrix_unnormalized, dim=1)
+        # TODO sentence_tensor = sentence_tensor[sentence_tensor > -1]
         emissions = emission_matrix[:, sentences_tensor].transpose(0, 1)
-        return emissions.log().sum(-1)  # TODO normalize??
+        return emissions.sum(-1)  # TODO normalize??
 
 
 class Scalar3DHMM(nn.Module):
@@ -80,33 +91,45 @@ class Scalar3DHMM(nn.Module):
     def forward(self, stories_tensor, story_length, length):
         assert(stories_tensor.shape[1] == story_length)
 
+        num_batches = stories_tensor.shape[0]
+
         # p(z0) across all states z0 (Z)
         state_priors = nn.functional.log_softmax(self.state_priors_unnormalized, dim=0)
-        # p(z0)*p(x0|z0) across all states z0, all first sentences x0 (N x Z)
-        scores = self.emission_model.log_p(stories_tensor[:, 0]) + state_priors
-        scores = scores.to(cuda)
+        # print("PRIOR", state_priors.exp())
 
-        # transitions = self.transition_model.log_p()
-        #for i in range(1, story_length):
-        #    scores = self.emission_model.log_p(stories_tensor[:, i]) + self.transition_model()
+        # p(z0)*p(x0|z0) across all states z0, all first sentences x0 (N x Z)
+        emissions = self.emission_model.log_p(stories_tensor[:, 0])
+        # print("EMITS INITIAL", emissions.exp())
+        scores = emissions + state_priors
+        # print("SCORE INITIAL", scores.exp())
+        # print("SCORE INITIAL", scores)
+
+        # TODO generate sparse matrix?
+        transitions = self.transition_model.log_p(self.num_states, self.xy_size)
+        # print("TRANS", transitions.exp())  # shape is num_states x num_states
 
         for i in range(1, story_length):
-            print(i, "SCORES", scores[:5]) # TODO testing
-            scores_next = torch.zeros(scores.shape).to(cuda)
-            for state in range(self.num_states):
-                print("\t", state)
-                for prev in range(self.num_states):
-                    scores_next[:, state] +=\
-                        self.transition_model.log_p(state, prev, index2coord(self.xy_size))\
-                        * self.emission_model.log_p(stories_tensor[:, i])[:,state]\
-                        * scores[:, prev]
-            scores = scores_next
+            # transitions = self.transition_model.log_p(self.num_states, index2coord(self.xy_size))
+            # print("\t\t", "TRANS", transitions.shape)  # shape is num_states x num_states
+            emissions = self.emission_model.log_p(stories_tensor[:, i])
+            # print(i, "EMITS", emissions.exp())  # shape is num_batches x num_states
+
+            # p(zi|zi-1)*p(xi|zi)*p(zi-1)
+            intermediate = (emissions.view(num_batches, -1, 1) + scores.view(num_batches, 1, -1))
+            # print(i, "INTER", intermediate.exp())
+            update = transitions + intermediate  # shape is num_batches x num_states x num_states
+            # print(i, "UPDATE", update.exp())
+
+            scores = update.logsumexp(-1)
+            # print(i, "SCORES", scores.exp())
+            # print(i, "SCORES", scores)
+
+        return scores.logsumexp(-1)
 
         # make the log sum
-        sums_log = scores.logsumexp(dim=2)
+        # sums_log = scores.logsumexp(dim=2)
         # calculate log probabilities
         # TODO log_probabilities = sums_log.gather(1, length.view(-1, 1) - 1)
-
         # return log_probabilities
 
     # emissions forward algorithm
--- 3d-hmm/test.py
+++ 3d-hmm/test.py
@@ -3,22 +3,21 @@ import torch
 from load_story_cloze import load_cloze_test
 from scalar_3d_hmm_model import Scalar3DHMM
 
-id = torch.cuda.current_device()
-cuda = "cuda:"+str(id)
-device = torch.device(cuda)
-
-
-stories = load_cloze_test().to(cuda)
+stories = load_cloze_test()
 print(stories.shape)
-print(stories.device)
 
-
-batches = stories[:20, :5].split(4)
+batches = stories[:, torch.tensor([0,1,2,3,4])].split(64)
 print(len(batches), batches[0].shape)
 
-model = Scalar3DHMM(15, 6, 5557).to(cuda)
+false_batch = stories[:64, torch.tensor([0,1,2,3,5])]
 
-for batch in batches[:1]:
-    with torch.no_grad():
-        model.forward(batch, 5, 0)
+model = Scalar3DHMM(7, 6, 5558)
+for epoch in range(50):
+    for idx, batch in enumerate(batches):
+        print(idx)
+        p = model.forward(batch, 5, 0)
+        with torch.no_grad():
+            print(p.mean())
+            print(model.forward(false_batch, 5, 0).mean())
+        p.sum(-1).backward()  # TODO is this correct?
 print("DONE")

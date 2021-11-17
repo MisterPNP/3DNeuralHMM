1c7190b1185aa2bc9faf99e730d6c3e3dcb7c681
MisterPNP <pnp@jgp.net>
2021-11-16 19:18:46 -0500

    Change prediction function to predicting full batch ranges, normalize model score during SGD learning

--- 3d-hmm/test.py
+++ 3d-hmm/test.py
@@ -4,27 +4,29 @@ from load_story_cloze import load_cloze_test
 from scalar_3d_hmm_model import Scalar3DHMM
 
 
-def score_prediction_single_story(model, story_index):
+def score_prediction_single_story(prediction_model, first_index, last_index):
     # input test.json
     test_stories = load_cloze_test()
     story = test_stories[story_index]
 
     # get true and false stories
-    true_story = story[(1, 2, 3, 4, 5)]
-    false_story = story[(1, 2, 3, 4, 6)]
+    true_stories = test_stories[:, torch.tensor([0, 1, 2, 3, 4])]
+    false_stories = test_stories[:, torch.tensor([0, 1, 2, 3, 5])]
 
     # score true and false stories
-    score_true = model.score(true_story, 5)
-    score_false = model.score(false_story, 5)
+    score_true = prediction_model.score(true_stories, 5)
+    score_false = prediction_model.score(false_stories, 5)
 
-    print("score for true ending: " + str(score_true))
-    print("score for false ending: " + str(score_false))
-
+    print("prediction scoring done.")
     # return a boolean to check if accurate prediction
     is_prediction_accurate = score_true > score_false
-    print("accurate prediction: " + str(is_prediction_accurate))
-
     return is_prediction_accurate
+
+
+def score_prediction_batch(first_index, last_index):
+    print("number of stories: " + str(last_index - first_index + 1))
+    count = score_prediction_single_story(model, first_index, last_index)
+    return count.float().mean()
 
 
 stories = load_cloze_test()
@@ -39,25 +41,25 @@ false_batch = stories[:64, torch.tensor([0, 1, 2, 3, 5])]
 model = Scalar3DHMM(6, 6, 5558)
 
 # SGD
-lr = 0.001
+learning_rate = 0.001
 for epoch in range(1):
     for idx, batch in enumerate(batches):
         print(idx)
-        p = model.score(batch, 5)
+        p = model.score(batch, 5) - model.emission_log_p(batch[:, -1]).logsumexp(-1)
         p.sum(-1).backward()
+
         with torch.no_grad():
             print(p.mean())
             print(model.score(false_batch, 5).mean())
 
-            model.emission_matrix_unnormalized += lr * model.emission_matrix_unnormalized.grad
+            model.emission_matrix_unnormalized += learning_rate * model.emission_matrix_unnormalized.grad
             model.emission_matrix_unnormalized.grad.zero_()
-            model.transition_matrix_unnormalized += lr * model.transition_matrix_unnormalized.grad
+            model.transition_matrix_unnormalized += learning_rate * model.transition_matrix_unnormalized.grad
             model.transition_matrix_unnormalized.grad.zero_()
-            model.state_priors_unnormalized += lr * model.state_priors_unnormalized.grad
+            model.state_priors_unnormalized += learning_rate * model.state_priors_unnormalized.grad
             model.state_priors_unnormalized.grad.zero_()
 print("DONE LEARNING")
 
-score_prediction_single_story(model, 0)
-
+print(score_prediction_batch(0, 1000))
 # TODO backward algorithm, instead of SGD
 # ...

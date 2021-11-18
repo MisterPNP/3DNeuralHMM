758cc0bf0c87ca92e78d6886f574f6c1f1773163
Alex Lewis <alexlewisnc@gmail.com>
2021-11-18 03:53:41 -0500

    Use only frequent words in token embeddings

--- 3d-hmm/load_story_cloze.py
+++ 3d-hmm/load_story_cloze.py
@@ -3,6 +3,8 @@ import string
 import json
 import pickle
 import os
+import urllib
+import zipfile
 
 import pandas as pd
 
@@ -13,18 +15,30 @@ import nltk.tokenize
 import torch
 import torchtext
 
+
 def get_processed_data():
-    # preprocess_cloze_test('cloze_test_test__spring2016 - cloze_test_ALL_test.csv', 'story_cloze_2016_test')
-    # preprocess_cloze_test('cloze_test_val__spring2016 - cloze_test_ALL_val.csv', 'story_cloze_2016_val')
-    preprocess_ROC_test('ROCStories__spring2016 - ROCStories_spring2016.csv', 'ROC_stories_2016')
+    required_downloads()
+    make_vocab()
+    preprocess_stories(
+        'cloze_test_test__spring2016 - cloze_test_ALL_test.csv',
+        'story_cloze_2016_test', make_cloze_sentences)
+    preprocess_stories(
+        'cloze_test_val__spring2016 - cloze_test_ALL_val.csv',
+        'story_cloze_2016_val', make_cloze_sentences)
+    preprocess_stories(
+        'ROCStories__spring2016 - ROCStories_spring2016.csv',
+        'ROC_stories_2016', make_roc_sentences)
+
 
 # HELPER FUNCTIONS
+
 
 def required_downloads():
     nltk.download('averaged_perceptron_tagger')
     nltk.download('punkt')
     nltk.download('stopwords')
     nltk.download('wordnet')
+
 
 def get_wordnet_pos(word):
     wordnet = nltk.corpus.wordnet
@@ -35,6 +49,7 @@ def get_wordnet_pos(word):
                 "V": wordnet.VERB,
                 "R": wordnet.ADV}
     return tag_dict.get(tag, wordnet.NOUN)
+
 
 def lemmatize(sentence):
     sentence = sentence.lower()
@@ -49,41 +64,53 @@ def lemmatize(sentence):
         lemmas.append(lemmatizer.lemmatize(token, pos))
     return lemmas
 
+
 # gets the data from file and pre-processes it
-def preprocess_cloze_test(data_name, output_name):
-    word2index = get_word2index()
+def preprocess_stories(data_name, output_name, make_sentences):
     filepath = '../data/' + data_name
     df = pd.read_csv(filepath)
+
+    word2index = get_word2index()
+    def lemma2index(lemma):
+        return word2index[lemma] if lemma in word2index else word2index["<unk>"]
+
     sequences = []  # each set of six sentences
     vocab = {}      # vocab counts across dataset
     for i in range(len(df)):
-        sentences = []
-        for sentence in df.iloc[i, 1:5]:
-            lemmas = list(map(word2index.get, lemmatize(sentence)))
-            for lemma in lemmas:
-                if lemma not in vocab:
-                    vocab[lemma] = 0
-                vocab[lemma] += 1
-            sentences.append(lemmas)
-        # make the correct ending come before the incorrect one
-        last_two = df.iloc[i, 5:7] if df.iloc[i, 7] == 1 else [df.iloc[i, 6], df.iloc[i, 5]]
-        for sentence in last_two:
-            lemmas = list(map(word2index.get, lemmatize(sentence)))
-            for lemma in lemmas:
-                if lemma not in vocab:
-                    vocab[lemma] = 0
-                vocab[lemma] += 1
-            sentences.append(lemmas)
-        sequences.append(sentences)
+        sequences.append(make_sentences(df.iloc[i], vocab, lemma2index))
 
     with open("../data/" + output_name + "_vocab.voc", "w") as out:
         for lemma, count in vocab.items():
             out.write(f"{lemma}\t{count}\n")
+    print("done with vocab")
 
-    with open("../data/" + output_name + ".json", "w") as out:
+    with open("../data/" + output_name + ".json", "w", encoding="utf-8") as out:
         json.dump(sequences, out, separators=(",", ":"))
+    print("done with json")
 
-    print("done")
+
+def make_cloze_sentences(row, vocab, lemma2index):
+    # make the correct ending come before the incorrect one
+    if row[7] == 2:
+        row[5], row[6] = row[6], row[5]
+    return make_sentences_from_row(row[1:7], vocab, lemma2index)
+
+
+def make_roc_sentences(row, vocab, lemma2index):
+    return make_sentences_from_row(row[2:7], vocab, lemma2index)
+
+
+def make_sentences_from_row(row, vocab, lemma2index):
+    sentences = []
+    for sentence in row:
+        lemmas = list(map(lemma2index, lemmatize(sentence)))
+        for lemma in lemmas:
+            if lemma not in vocab:
+                vocab[lemma] = 0
+            vocab[lemma] += 1
+        sentences.append(lemmas)
+    return sentences
+
 
 # load the data from file if it has already been written by preprocess_cloze_test()
 def load_cloze_file(filename):
@@ -99,34 +126,6 @@ def load_cloze_file(filename):
                 story[i] += [-1] * (sentence_length - len(sentence))
         return torch.tensor(stories)
 
-# gets the data from file and pre-processes it
-def preprocess_ROC_test(data_name, output_name):
-    word2index = get_word2index()
-    filepath = '../data/' + data_name
-    df = pd.read_csv(filepath)
-
-    sequences = []  # each set of six sentences
-    vocab = {}  # vocab counts across dataset
-    for i in range(len(df)):
-        sentences = []
-        for sentence in df.iloc[i, 2:7]:
-            lemmas = list(map(word2index.get, lemmatize(sentence)))
-            for lemma in lemmas:
-                if lemma not in vocab:
-                    vocab[lemma] = 0
-                vocab[lemma] += 1
-            sentences.append(lemmas)
-        sequences.append(sentences)
-
-    with open("../data/" + output_name + "_vocab.voc", "w") as out:
-        for lemma, count in vocab.items():
-            out.write(f"{lemma}\t{count}\n")
-    print("done with vocab")
-
-    with open("../data/" + output_name + ".json", "w", encoding="utf-8") as out:
-        json.dump(sequences, out, separators=(",", ":"))
-    print("done with json")
-
 
 def load_roc_test():
     return load_cloze_file("ROC_stories_2016.json")
@@ -139,18 +138,60 @@ def load_cloze_test():
 def load_cloze_valid():
     return load_cloze_file("story_cloze_2016_val.json")
 
+
+def download_glove_embeddings():
+    filename = '../data/glove.6B.zip'
+    if not os.path.exists(filename):
+        print("downloading GLOVE embeddings")
+        urllib.request.urlretrieve("http://nlp.stanford.edu/data/glove.6B.zip", filename)
+
+    print("extracting GLOVE embeddings")
+    with zipfile.ZipFile(filename, 'r') as zip_ref:
+        # for fileinfo in zip_ref.infolist():
+        #     filename = fileinfo.filename
+        #     outputfile = open(filename, "wb")
+        #     with zip_ref.open(filename) as inputfile:
+        #         outputfile.write(inputfile.read())
+        zip_ref.extractall('../data/glove')
+
+
 def make_vocab():
-    filename = "../data/glove/glove.6B.100d.txt"
-    # TODO if not os.path.exists(filename):
-    #     download_glove_embeddings()
+    glove2index = {}
+    with open("../data/vocab_frequent.voc", "r") as file:
+        for i, line in enumerate(file):
+            glove_index = int(line.split()[0])
+            glove2index[glove_index] = i + 1
 
     word2index = {}
-    with open(filename, "r", encoding="utf-8") as file:
+    index2word = {}
+    index2tensor = {}
+    glove = "../data/glove/glove.6B.100d.txt"
+    if not os.path.exists(glove):
+        download_glove_embeddings()
+    with open(glove, "r", encoding="utf-8") as file:
         for i, line in enumerate(file):
-            word2index[line.split()[0]] = i
+            if i in glove2index:
+                word = line.split()[0]
+                index = glove2index[i]
+                tensor = torch.tensor(list(map(float, line.split()[1:])))
+                word2index[word] = index
+                index2word[index] = word
+                index2tensor[index] = tensor
+
+    tensors = torch.stack(list(map(
+        lambda i: index2tensor[i + 1], range(len(index2tensor))
+    )))
+    # use average of tensors as <unk> embedding
+    tensors = torch.cat([tensors.mean(0).unsqueeze(0), tensors])
+    word2index["<unk>"] = 0
+    index2word[0] = "<unk>"
 
     with open("../data/word2index.pkl", "wb") as file:
         pickle.dump(word2index, file)
+    with open("../data/index2word.pkl", "wb") as file:
+        pickle.dump(index2word, file)
+    torch.save(tensors, "../data/word_tensors.tensor")
+
 
 def get_word2index():
     filename = "../data/word2index.pkl"
@@ -158,6 +199,4 @@ def get_word2index():
         make_vocab()
 
     with open(filename, "rb") as file:
-        w2i = pickle.load(file)
-        w2i[None] = 1  # TODO use an averaged vector for unk
-        return w2i
+        return pickle.load(file)

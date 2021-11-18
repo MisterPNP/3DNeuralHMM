Fix gradient descent logging

3d-hmm/test.py
@@ -38,7 +38,7 @@ token_embeddings = torch.stack(token_embeddings)
 
 model = Neural3DHMM(6, 6, len(token_embeddings), token_embeddings=token_embeddings)
 learning_rate = 1e-6
-num_epochs = 10
+num_epochs = 2
 
 
 analysis = train(model, batches, lr=learning_rate, num_epochs=num_epochs,

3d-hmm/train.py
@@ -9,24 +9,37 @@ def loss(model):
 
 
 def train(model, batches, lr=1e-3, num_epochs=3, valid_batches=None, accuracy_function=None):
-    analysis = {'test_loss': [], 'valid_loss': [], 'accuracy': []}
+    analysis = {'test_loss': []}
+    if valid_batches is not None:
+        analysis['valid_loss'] = []
+    if accuracy_function is not None:
+        analysis['accuracy'] = []
 
     # SGD
     for epoch in range(num_epochs):
+        print()
+        print("epoch", epoch)
+
+        analysis['test_loss'].append([])
+        if 'valid_loss' in analysis:
+            analysis['valid_loss'].append([])
+        if 'accuracy' in analysis:
+            analysis['accuracy'].append([])
+
         for idx, batch in enumerate(batches):
-            print(idx)
+            print("batch", idx)
             p = model.score(batch, 5)  # - model.emission_log_p(batch[:, -1]).logsumexp(-1)
             p.sum(-1).backward()
 
             with torch.no_grad():
-                analysis['test_loss'].append(p.mean())
-                print("TEST_LOSS", analysis['test_loss'][-1])
+                analysis['test_loss'][-1].append(p.mean())
+                print("TEST_LOSS", analysis['test_loss'][-1][-1])
                 if valid_batches is not None:
-                    analysis['valid_loss'].append(tuple(map(loss(model), valid_batches)))
-                    print("VALID_LOSS", analysis['valid_loss'][-1])
+                    analysis['valid_loss'][-1].append(tuple(map(loss(model), valid_batches)))
+                    print("VALID_LOSS", analysis['valid_loss'][-1][-1])
                 if accuracy_function is not None:
-                    analysis['accuracy'].append(accuracy_function(model))
-                    print("ACCURACY", analysis['accuracy'][-1])
+                    analysis['accuracy'][-1].append(accuracy_function(model))
+                    print("ACCURACY", analysis['accuracy'][-1][-1])
 
                 for parameter in model.parameters():
                     # print(parameter.grad.norm())
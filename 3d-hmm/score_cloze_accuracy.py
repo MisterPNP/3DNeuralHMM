Fix gradient descent logging

3d-hmm/score_cloze_accuracy.py
@@ -1,22 +1,22 @@
 
 import torch
 
-from load_story_cloze import load_cloze_test
+from load_story_cloze import load_cloze_valid
 
 
 def score_prediction_batch(model, first=0, last=-2):
     test_stories = load_cloze_valid()[first:(last + 1)]
-    print("number of stories: " + len(test_stories))
+    # print("number of stories:", len(test_stories))
 
     # get true and false stories
     true_stories = test_stories[:, torch.tensor([0, 1, 2, 3, 4])]
     false_stories = test_stories[:, torch.tensor([0, 1, 2, 3, 5])]
 
     # score true and false stories
-    score_true = prediction_model.score(true_stories, 5)
-    score_false = prediction_model.score(false_stories, 5)
+    score_true = model.score(true_stories, 5)
+    score_false = model.score(false_stories, 5)
+    # print("prediction scoring done.")
 
-    print("prediction scoring done.")
     # return a boolean to check if accurate prediction
     is_prediction_accurate = score_true > score_false
 

3d-hmm/test.py
@@ -12,11 +12,11 @@ from neural_3d_hmm_model import Neural3DHMM
 # TODO: Remove low-frequency tokens?
 
 
-stories = load_roc_test()
+stories = load_cloze_test()
 
-batch_size = 1 + len(stories) // 5
+batch_size = 1000
 batches = stories[:, torch.tensor([0, 1, 2, 3, 4])].split(batch_size)
-false_batch = stories[:batch_size, torch.tensor([0, 1, 2, 3, 5])]
+false_batch = stories[:, torch.tensor([0, 1, 2, 3, 5])]
 
 
 # model = Gradient3DHMM(6, 6, 11571)
@@ -36,16 +36,17 @@ for _, row in vocab.iterrows():
     token_embeddings.append(emb)
 token_embeddings = torch.stack(token_embeddings)
 
-model = Neural3DHMM(6, 6, 11571, token_embeddings=token_embeddings)
+model = Neural3DHMM(6, 6, len(token_embeddings), token_embeddings=token_embeddings)
 learning_rate = 1e-6
 num_epochs = 10
 
 
 analysis = train(model, batches, lr=learning_rate, num_epochs=num_epochs,
-      valid_batches=[false_batch], accuracy_function=score_prediction_batch)
+      valid_batches=[], accuracy_function=None)
 
+print()
 print("DONE LEARNING")
-
+print()
 print("TEST_LOSS", analysis['test_loss'])
 print("VALID_LOSS", analysis['valid_loss'])
-print("ACCURACY", analysis['accuracy'])
+print("ACCURACY", score_prediction_batch(model))

3d-hmm/train.py
@@ -1,16 +1,15 @@
 import torch
 
+
 def loss(model):
     def f(batch):
         p = model.score(batch, 5)
         return p.mean()
     return f
 
+
 def train(model, batches, lr=1e-3, num_epochs=3, valid_batches=None, accuracy_function=None):
-    analysis = {}
-    analysis['test_loss'] = []
-    analysis['valid_loss'] = []
-    analysis['accuracy'] = []
+    analysis = {'test_loss': [], 'valid_loss': [], 'accuracy': []}
 
     # SGD
     for epoch in range(num_epochs):
@@ -21,10 +20,13 @@ def train(model, batches, lr=1e-3, num_epochs=3, valid_batches=None, accuracy_fu
 
             with torch.no_grad():
                 analysis['test_loss'].append(p.mean())
+                print("TEST_LOSS", analysis['test_loss'][-1])
                 if valid_batches is not None:
-                    analysis['valid_loss'] = tuple(map(loss(model), valid_batches))
+                    analysis['valid_loss'].append(tuple(map(loss(model), valid_batches)))
+                    print("VALID_LOSS", analysis['valid_loss'][-1])
                 if accuracy_function is not None:
-                    analysis['accuracy'] = accuracy_function(model)
+                    analysis['accuracy'].append(accuracy_function(model))
+                    print("ACCURACY", analysis['accuracy'][-1])
 
                 for parameter in model.parameters():
                     # print(parameter.grad.norm())